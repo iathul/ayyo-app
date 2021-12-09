6336bb58a79996b20bcf4e81caaa65ac9e147854
Athul Joy <a.athuljoy@gmail.com>
2021-12-09 20:21:16 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// Todo
+const express = require('express')
+
+//Create an express app
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))   
+app.use(express.json())
+
+const PORT = 5000
+
+app.listen(PORT, 
+    console.log(`Server Runnig at PORT: ${PORT}`)
+)
