355689d700235fc8c232dff98ca814ab5a3fed4e
Athul Joy <a.athuljoy@gmail.com>
2021-12-12 20:59:14 +0530

    Add Eslint

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-"use strict"
-
 // Load environment variables
-require('dotenv').config()
-
-// Import database connection
-const connectDB = require('./config/db')
-
-// Start db
-connectDB()
+require('dotenv').config();
 
 // Import packages
-const express = require('express')
+const express = require('express');
+
+// Import database connection
+const connectDB = require('./config/db');
+
+// Start db
+connectDB();
 
 // Create an express app
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({ extended: false }))   
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/users', require('./routes/user'))
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/user'));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 // Start server
-app.listen(PORT, 
-    console.log(`Server Runnig at PORT: ${PORT}`)
-)
+app.listen(
+  PORT,
+  // eslint-disable-next-line no-console
+  console.log(`Server Runnig at PORT: ${PORT}`),
+);
