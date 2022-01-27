726902e3e84f9122b21fa7c2a2949dd9c87c6a43
Athul Joy <a.athuljoy@gmail.com>
2022-01-27 19:50:53 +0530

    Add status field

--- models/package.js
+++ models/package.js
@@ -2,19 +2,27 @@ const mongoose = require('mongoose');
 
 const { ObjectId } = mongoose.Schema;
 
-const packageSchema = new mongoose.Schema({
-  user: {
-    type: ObjectId,
-    ref: 'User',
+const packageSchema = new mongoose.Schema(
+  {
+    user: {
+      type: ObjectId,
+      ref: 'User',
+    },
+    packageId: {
+      type: String,
+      unique: true,
+    },
+    files: {
+      type: Array,
+      default: [],
+    },
+    status: {
+      type: String,
+      enum: ['package created', 'package downloaded'],
+      default: 'created',
+    },
   },
-  packageId: {
-    type: String,
-    unique: true,
-  },
-  files: {
-    type: Array,
-    default: [],
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Package', packageSchema);
