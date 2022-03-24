6d5930b29b56a6a6391814ee3453ec7b11a38cec
Athul Joy <a.athuljoy@gmail.com>
2022-03-24 17:02:12 +0530

    Optimize code

--- controllers/file.js
+++ controllers/file.js
@@ -6,14 +6,12 @@ const moment = require('moment');
 const path = require('path');
 const { PassThrough } = require('stream');
 const Package = require('../models/package');
-const { storagePath, s3Storage } = require('../config/multer');
+const { s3Storage } = require('../config/multer');
 const s3 = require('../config/S3Config');
 
 exports.uploadFiles = (req, res) => {
   const fileLoc = nanoid(6);
-  const storage = process.env.NODE_ENV === 'development'
-    ? storagePath(`files/${fileLoc}`)
-    : s3Storage(fileLoc);
+  const storage = s3Storage(fileLoc);
   const upload = multer({ storage }).array('fileData');
 
   upload(req, res, async (err) => {
@@ -41,9 +39,7 @@ exports.uploadFiles = (req, res) => {
     });
 
     // Package destination
-    const packageDestination = process.env.NODE_ENV === 'development'
-      ? `./media/files/${fileLoc}`
-      : `${fileLoc}`;
+    const packageDestination = process.env.NODE_ENV === `${fileLoc}`;
 
     // Create and save packge
     const packageId = `package_${nanoid()}`;
@@ -105,85 +101,49 @@ exports.dowloadPackage = async (req, res) => {
     // Download package with single file
     if (filePackage.files.length === 1) {
       const filePath = `${filePackage.package_destination}/${filePackage.files[0].originalname}`;
-      if (process.env.NODE_ENV === 'development') {
-        res.download(filePath, (err) => {
-          if (err) {
-            return res.status(500).json({
-              error: 'Unable to download the package. please try again',
-            });
-          }
-        });
-      } else {
-        // Download single file from S3
+
+      // Download single file from S3
+      const options = {
+        Bucket: 'ayyo-file-storage',
+        Key: filePath,
+      };
+      res.attachment(filePath);
+      const fileStream = s3.getObject(options).createReadStream();
+      fileStream.pipe(res);
+    }
+    if (filePackage.files.length > 1) {
+      // Download package with multiple files
+      const fileDir = `./downloads/${filePackage.package_destination}`;
+      if (!fs.existsSync(fileDir)) {
+        fs.mkdirSync(fileDir, { recursive: true });
+      }
+      let complete = 0;
+      const zip = new AdmZip();
+      const downloadPath = path.join(process.cwd(), fileDir);
+      filePackage.files.forEach((file) => {
+        const filePath = `${filePackage.package_destination}/${file.originalname}`;
         const options = {
           Bucket: 'ayyo-file-storage',
           Key: filePath,
         };
-        res.attachment(filePath);
         const fileStream = s3.getObject(options).createReadStream();
-        fileStream.pipe(res);
-      }
-    }
-    if (filePackage.files.length > 1) {
-      if (process.env.NODE_ENV === 'development') {
-        // Download package with multiple files
-        const fileDir = `./downloads/${filePackage.package_destination}`;
-        if (!fs.existsSync(fileDir)) {
-          fs.mkdirSync(fileDir, { recursive: true });
-        }
-        let complete = 0;
-        const zip = new AdmZip();
-        const downloadPath = path.join(process.cwd(), fileDir);
-        filePackage.files.forEach((file) => {
-          const filePath = `${filePackage.package_destination}/${file.originalname}`;
-          const options = {
-            Bucket: 'ayyo-file-storage',
-            Key: filePath,
-          };
-          const fileStream = s3.getObject(options).createReadStream();
-          const writeStream = fs.createWriteStream(
-            `${downloadPath}/${file.originalname}`
-          );
-          fileStream.pipe(writeStream);
-          writeStream.on('finish', () => {
-            complete += 1;
-            if (complete === filePackage.files.length) {
-              zip.addLocalFolder(fileDir, filePackage.packageId);
-              const zipFile = zip.toBuffer();
-              res.attachment(`${Date.now()}.zip`);
-              const zipStream = new PassThrough();
-              zipStream.end(zipFile);
-              zipStream.pipe(res);
-              fs.rmSync(fileDir, { recursive: true, force: true });
-            }
-          });
-        });
-      } else {
-        // Create zip for package with multiple files
-        const zipFile = `${Date.now()}.zip`;
-        const zip = new AdmZip();
-        zip.addLocalFolder(filePackage.package_destination);
-
-        // Zip output folder
-        const dir = './downloads';
-        if (!fs.existsSync(dir)) {
-          fs.mkdirSync(dir);
-        }
-        // Save zip and download zip
-        const zipFilePath = path.join(process.cwd(), dir);
-        fs.writeFileSync(`${zipFilePath}/${zipFile}`, zip.toBuffer());
-        res.download(`${zipFilePath}/${zipFile}`, async (err) => {
-          if (err) {
-            console.log(err);
+        const writeStream = fs.createWriteStream(
+          `${downloadPath}/${file.originalname}`
+        );
+        fileStream.pipe(writeStream);
+        writeStream.on('finish', () => {
+          complete += 1;
+          if (complete === filePackage.files.length) {
+            zip.addLocalFolder(fileDir, filePackage.packageId);
+            const zipFile = zip.toBuffer();
+            res.attachment(`${Date.now()}.zip`);
+            const zipStream = new PassThrough();
+            zipStream.end(zipFile);
+            zipStream.pipe(res);
+            fs.rmSync(fileDir, { recursive: true, force: true });
           }
-          // Delete zip once download is complete
-          await fs.unlink(`${zipFilePath}/${zipFile}`, (error) => {
-            if (error) {
-              console.log(error);
-            }
-          });
         });
-      }
+      });
     }
   } catch (error) {
     console.log(error);
