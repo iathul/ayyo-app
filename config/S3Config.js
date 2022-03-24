d6f0523c922e3f8bb5becf9759121be70bf0863d
Athul Joy <a.athuljoy@gmail.com>
2022-03-24 18:33:32 +0530

    Update env

--- config/S3Config.js
+++ config/S3Config.js
@@ -5,7 +5,7 @@ const S3 = new AWS.S3();
 S3.config.update({
   secretAccessKey: process.env.AWS_S3_ACCESS_KEY,
   accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-  region: 'Asia Pacific (Mumbai) ap-south-1',
+  region: process.env.AWS_S3_REGION,
 });
 
 module.exports = S3;
