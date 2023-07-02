9d9a9e8c0c8c23b870d8ff4114e5f56ab3d79152
Athul Joy <a.athuljoy@gmail.com>
2023-07-02 19:58:04 +0530

    Add s3 bucket name

--- config/S3Config.js
+++ config/S3Config.js
@@ -3,9 +3,9 @@ const AWS = require('aws-sdk');
 const S3 = new AWS.S3();
 
 S3.config.update({
-  secretAccessKey: process.env.AWS_S3_ACCESS_KEY,
-  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-  region: process.env.AWS_S3_REGION,
+  secretAccessKey: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  region: process.env.AWS_REGION
 });
 
 module.exports = S3;
