a1d663c1f494c6af9b42a83e0b62e64581ea9c95
Athul Joy <a.athuljoy@gmail.com>
2023-07-08 19:08:21 +0530

    Add util function to generate token

--- utils/token.js
+++ utils/token.js
@@ -1,6 +1,14 @@
 const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
 
 exports.createVerificationToken = () => {
   const token = crypto.randomBytes(32).toString('hex')
   return token
+}
+
+exports.generateAccessRefreshToken = (user, type) => {
+  const secret = process.env[`${type.toUpperCase()}_TOKEN_SECRET`]
+  const expiresIn = process.env[`${type.toUpperCase()}_TOKEN_EXPIRY`]
+
+  return jwt.sign({ _id: user._id }, secret, { expiresIn })
 }
