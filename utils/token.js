6c2fd7a63e02046a2286904adefbf4e7c6b3046f
Athul Joy <a.athuljoy@gmail.com>
2023-07-08 20:37:54 +0530

    Remove white space

--- utils/token.js
+++ utils/token.js
@@ -9,6 +9,5 @@ exports.createVerificationToken = () => {
 exports.generateAccessRefreshToken = (user, type) => {
   const secret = process.env[`${type.toUpperCase()}_TOKEN_SECRET`]
   const expiresIn = process.env[`${type.toUpperCase()}_TOKEN_EXPIRY`]
-
   return jwt.sign({ _id: user._id }, secret, { expiresIn })
 }
